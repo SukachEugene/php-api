f71017f7f2e51a78878931c605a5e55fe0d74e87
UJ <sukach.eugene@gmail.com>
2024-06-12 15:14:02 +0300

    responce header values is showed individually

--- index.php
+++ index.php
@@ -2,19 +2,27 @@
 
 $ch = curl_init();
 
-// curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$headers = [
+    "Authorization: Client-ID gcpjTjclC_LGAGjhA6OK4O9Q77cH5O3FdoaQpzJPKjQ"
+];
+
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q=Poltava&appid=a1eb3d0585788328d66af5ddb090c53e",
-    CURLOPT_RETURNTRANSFER => true
+    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_HEADER => true
 ]);
 
 $response = curl_exec($ch);
 
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+$content_length = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 
 curl_close($ch);
 
 echo $status_code, "\n";
+echo $content_type, "\n";
+echo $content_length, "\n";
 
 echo $response, "\n";
