820648d84faa5481050babc27082001b6a401073
UJ <sukach.eugene@gmail.com>
2024-06-12 15:43:14 +0300

    responce headers converted into associative array

--- index.php
+++ index.php
@@ -6,23 +6,39 @@ $headers = [
     "Authorization: Client-ID gcpjTjclC_LGAGjhA6OK4O9Q77cH5O3FdoaQpzJPKjQ"
 ];
 
+$response_headers = [];
+
+$header_callback = function($ch, $header) use (&$response_headers) {
+
+    $len = strlen($header);
+
+    $parts = explode(":", $header, 2);
+
+    // not valid header
+    if (count($parts) < 2) {
+        return $len;
+    }
+
+    $response_headers[$parts[0]] = trim($parts[1]);
+
+    return $len;
+};
+
 curl_setopt_array($ch, [
     CURLOPT_URL => "https://api.unsplash.com/photos/random",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_HEADER => true
+    CURLOPT_HEADERFUNCTION => $header_callback
 ]);
 
 $response = curl_exec($ch);
 
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-$content_length = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 
 curl_close($ch);
 
 echo $status_code, "\n";
-echo $content_type, "\n";
-echo $content_length, "\n";
+
+print_r($response_headers);
 
 echo $response, "\n";
