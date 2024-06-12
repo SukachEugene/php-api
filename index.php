e789f24adcae79a76eb5bc2168f5a37035307083
UJ <sukach.eugene@gmail.com>
2024-06-12 18:20:34 +0300

    finished preparing and start to create app

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
 
-require __DIR__ . "/vendor/autoload.php";
-
-$client = new GuzzleHttp\Client;
-
-$response = $client->request("GET", "https://api.github.com/user/repos", [
-    "headers" => [
-        "Authorization" => 'token TOKEN_HERE',
-        "User-Agent" => "SukachEugene"
-    ]
-]);
-
-echo $response->getStatusCode(), "\n";
-
-echo $response->getHeader("content-type")[0], "\n";
-
-echo substr($response->getBody(), 0 ,200), "...\n";
+echo "Hello world!";
