77f556e5466f978865249a409909e3748785e7a8
UJ <sukach.eugene@gmail.com>
2024-06-13 19:29:41 +0300

    database is connected + creted methods for retrieving data

--- src/TaskController.php
+++ src/TaskController.php
@@ -2,13 +2,19 @@
 
 class TaskController
 {
+    public function __construct(private TaskGateway $gateway)
+    {
+        
+    }
+
     public function processRequest(string $method, ?string $id): void
     {
         if ($id === null) {
 
             if ($method == "GET") {
 
-                echo "index";
+                echo json_encode($this->gateway->getAll());
+
             } elseif ($method == "POST") {
 
                 echo "create";
@@ -16,11 +22,21 @@ class TaskController
 
                 $this->respondMethodNotAllowed("GET, POST");
             }
-            
+
         } else {
+
+            $task = $this->gateway->get($id);
+
+            if ($task === false) {
+
+                $this->respondNotFound($id);
+                return;
+            }
+
             switch ($method) {
+                
                 case "GET":
-                    echo "show $id";
+                    echo json_encode($task);
                     break;
 
                 case "PATCH":
@@ -41,5 +57,11 @@ class TaskController
     {
         http_response_code(405);
         header("Allow: {$allowed_methods}");
+    }
+
+    private function respondNotFound(string $id): void
+    {
+        http_response_code(404);
+        echo json_encode(["message" => "Task with ID $id not found"]);
     }
 }
