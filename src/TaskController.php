d747e221f7fff4bc3145bae73385ec497abd66f3
UJ <sukach.eugene@gmail.com>
2024-06-14 13:52:30 +0300

    added basic CRUD functions

--- src/TaskController.php
+++ src/TaskController.php
@@ -4,7 +4,6 @@ class TaskController
 {
     public function __construct(private TaskGateway $gateway)
     {
-        
     }
 
     public function processRequest(string $method, ?string $id): void
@@ -14,15 +13,25 @@ class TaskController
             if ($method == "GET") {
 
                 echo json_encode($this->gateway->getAll());
-
             } elseif ($method == "POST") {
 
-                echo "create";
+                $data = (array) json_decode(file_get_contents("php://input"), true);
+
+                $errors = $this->getValidationErrors($data);
+
+                if (!empty($errors)) {
+
+                    $this->respondUnprocessableEntity($errors);
+                    return;
+                }
+
+                $id = $this->gateway->create($data);
+
+                $this->respondCreated($id);
             } else {
 
                 $this->respondMethodNotAllowed("GET, POST");
             }
-
         } else {
 
             $task = $this->gateway->get($id);
@@ -34,23 +43,43 @@ class TaskController
             }
 
             switch ($method) {
-                
+
                 case "GET":
                     echo json_encode($task);
                     break;
 
                 case "PATCH":
-                    echo "update $id";
+
+                    $data = (array) json_decode(file_get_contents("php://input"), true);
+
+                    $errors = $this->getValidationErrors($data, false);
+    
+                    if (!empty($errors)) {
+    
+                        $this->respondUnprocessableEntity($errors);
+                        return;
+                    }
+
+                    $rows = $this->gateway->update($id, $data);
+                    echo json_encode(["message" => "Task updated", "rows" => $rows]);
                     break;
 
                 case "DELETE":
-                    echo "delete $id";
+                    
+                    $rows = $this->gateway->delete($id);
+                    echo json_encode(["message" => "Task deleted", "rows" => $rows]);
                     break;
 
                 default:
                     $this->respondMethodNotAllowed("GET, PATCH, DELETE");
             }
         }
+    }
+
+    private function respondUnprocessableEntity(array $errors): void
+    {
+        http_response_code(422);
+        echo json_encode(["errors" => $errors]);
     }
 
     private function respondMethodNotAllowed(string $allowed_methods): void
@@ -63,5 +92,31 @@ class TaskController
     {
         http_response_code(404);
         echo json_encode(["message" => "Task with ID $id not found"]);
+    }
+
+    private function respondCreated(string $id): void
+    {
+        http_response_code(201);
+        echo json_encode(["message" => "Task created", "id" => $id]);
+    }
+
+    private function getValidationErrors(array $data, $is_new = true): array
+    {
+        $errors = [];
+
+        if ($is_new && empty($data["name"])) {
+
+            $errors[] = "name is required";
+        }
+
+        if (!empty($data["priority"])) {
+
+            if (filter_var($data["priority"], FILTER_VALIDATE_INT) === false) {
+
+                $errors[] = "priority must be an integer";
+            }
+        }
+
+        return $errors;
     }
 }
